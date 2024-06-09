ab2c95a57eb83b37ab6ebf5b55ddbb9110c33277
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-06-09 17:38:55 +0200

    GEM-90 add new services

--- src/main/kotlin/pl/edu/agh/gem/config/AcceptedHost.kt
+++ src/main/kotlin/pl/edu/agh/gem/config/AcceptedHost.kt
@@ -2,4 +2,8 @@ package pl.edu.agh.gem.config
 
 object AcceptedHost {
     const val AUTHENTICATOR = "web.authenticator.com"
+    const val GROUP_MANAGER = "web.group-manager.com"
+    const val CURRENCY_MANAGER = "web.currency-manager.com"
+    const val EXPENSE_MANAGER = "web.expense-manager.com"
+    const val ATTACHMENT_STORE = "web.attachment-store.com"
 }
