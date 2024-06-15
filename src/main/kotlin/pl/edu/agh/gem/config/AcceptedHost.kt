35be2960f8f0af487b735604cd736a050a4ee056
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-06-15 06:58:34 +0200

    GEM-90 pr fixes

--- src/main/kotlin/pl/edu/agh/gem/config/AcceptedHost.kt
+++ src/main/kotlin/pl/edu/agh/gem/config/AcceptedHost.kt
@@ -1,9 +1,9 @@
 package pl.edu.agh.gem.config
 
 object AcceptedHost {
-    const val AUTHENTICATOR = "web.authenticator.com"
-    const val GROUP_MANAGER = "web.group-manager.com"
-    const val CURRENCY_MANAGER = "web.currency-manager.com"
-    const val EXPENSE_MANAGER = "web.expense-manager.com"
-    const val ATTACHMENT_STORE = "web.attachment-store.com"
+    const val AUTHENTICATOR = "gem.web.authenticator.com"
+    const val GROUP_MANAGER = "gem.web.group-manager.com"
+    const val CURRENCY_MANAGER = "gem.web.currency-manager.com"
+    const val EXPENSE_MANAGER = "gem.web.expense-manager.com"
+    const val ATTACHMENT_STORE = "gem.web.attachment-store.com"
 }
